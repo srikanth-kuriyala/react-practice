5d9be28fe5f25b83ffcb85fde29d091399c8aa64
Srikanth Kuriyala <srikanth.kuriyala@multichoice.com>
2021-10-21 12:18:37 +0530

    React hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import Home from "./home";
+import Menu from "./menu/menu";
+import Page from "./page";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Menu />
+        <Switch>
+          <Route path="/:slug">
+            <Page />
+          </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
